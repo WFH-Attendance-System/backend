1394c6c5c8f863dac252baff38aae43a8349ba94
Chery <cherylene40@gmail.com>
2025-02-09 14:30:47 +0700

    Fix: add cors

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(
     cors({
         origin: "http://localhost:5173",
         credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 // Serve static files
